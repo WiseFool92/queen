20786cc415cc2fe31af40a30d310854973498017
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-04 15:22:12 -0700

    turn data into string arrays

--- queen.cs
+++ queen.cs
@@ -4,21 +4,23 @@ class Queen
 {
   static void Main()
   {
-    Cordinates("queen");
-    Cordinates("pawn");
-    Attack("1", "3", "1", "5");
+    string[] queens = Cordinates("queen");
+    string[] other = Cordinates("pawn");
+    Attack(queens, other);
   }
-  static void Cordinates(string piece)
+  static string[] Cordinates(string piece)
   {
-    Console.WriteLine("What is the x position of " + piece  + "?");
+    Console.WriteLine("What is the x position of " + piece + "?");
     string yourXCord = Console.ReadLine();
     Console.WriteLine("What is the y position of " + piece + "?");
     string yourYCord = Console.ReadLine();
     Console.WriteLine("Your " + piece + " x pos is " + yourXCord + " & your " + piece + " y pos is " + yourYCord + ".");
+    string[] bothCord = { yourXCord, yourYCord };
+    return bothCord;
   }
-  static void Attack(string xCordQ, string yCordQ, string xCordO, string yCordO)
+  static void Attack(string[] queens, string[] other)
   {
-    if (xCordQ == xCordO || yCordQ == yCordO) 
+    if (queens[0] == other[0] || queens[1] == other[1])
     {
       Console.WriteLine("The Queen can attack.");
     }
